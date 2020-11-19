0321883a244862323cf29805e1aeb89d5add980b
Ramy-arch <hadimramy60@gmail.com>
2020-11-19 23:23:40 +0100

    new commit

--- cb.cpp
+++ cb.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include<iomanip>
 using namespace std;
 
-int main() {
-  cout << "Hello World!";
+class Student{
+  public:
+  string Name;
+  int ID;
+  int Year;
+
+  Student(string a,int b,int c); 
+};
+
+Student::Student(string a,int b,int c){
+  Name = a;
+  ID = b;
+  Year = c;
+}
+
+int main () {
+  Student obj("Ramy",12459875,2020);
+  cout<<"Nom : "<<obj.Name<<"\nID : "<<obj.ID<<"\nannee : "<<obj.Year<<endl;
+  
   return 0;
 }
+
